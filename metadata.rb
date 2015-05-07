03050484d1e75f99f61a5160cf75cd7adb33af81
Peter Mitchell <peterjmit@gmail.com>
2015-05-07 12:50:48 -0400

    Bump to 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'pete@peterjmit.com'
 license          'MIT'
 description      'Installs/Configures ssl_cert'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 %w{debian ubuntu redhat centos fedora scientific amazon}.each do |os|
   supports os
